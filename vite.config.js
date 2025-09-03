33355861870d0dcc3051684d4d14aadfc960b3e2
yosuahres <haresyosuaa@gmail.com>
2025-09-03 21:56:31 +0700

    chore: update dev server port

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    port: 6969, // Change this to your desired port
+  },
 });
